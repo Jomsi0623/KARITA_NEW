8bbadaf9d62b9e4ac9755e9541fb6e200de94cc5
Ivan Patricio <105693357+ivanPee@users.noreply.github.com>
2025-04-10 13:23:03 +0800

    Update tts_component

--- tts_component.py
+++ tts_component.py
@@ -1,17 +1,20 @@
-# tts_engine.py
-
-import sounddevice as sd
-import numpy as np
-from TTS.api import TTS
+import subprocess
 
 class TextToSpeechEngine:
-    def __init__(self, model_name="tts_models/en/ljspeech/tacotron2-DDC"):
-        print("[TTS] Initializing Text-to-Speech Engine...")
-        self.tts = TTS(model_name, progress_bar=False).to("cpu")
-        print("[TTS] TTS Engine initialized.")
+    def __init__(self, voice='en+f3+klatt3', speed=130, pitch=60):
+        self.voice = voice
+        self.speed = speed
+        self.pitch = pitch
+        #PATH NA DI KA espeak MO MEG ISLI LANG
+        self.espeak_path = r"C:\Program Files (x86)\eSpeak\command_line\espeak.exe"
+        print(f"[TTS] Using eSpeak at: {self.espeak_path}")
 
     def speak(self, text):
         print(f"[TTS] Speaking: {text}")
-        audio_output = self.tts.tts(text=text)
-        sd.play(np.array(audio_output), samplerate=22050)
-        sd.wait()
+        subprocess.run([
+            self.espeak_path,
+            f"-v{self.voice}",
+            f"-s{self.speed}",
+            f"-p{self.pitch}",
+            text
+        ])
